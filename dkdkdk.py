b3e6fef9079b2e72be8d85f785459b8f2d9d0a0c
domogod <163220173+domogod@users.noreply.github.com>
2024-03-13 11:13:00 +0900

    Add files via upload

--- dkdkdk.py
+++ dkdkdk.py
@@ -1,8 +1,9 @@
-word = "man"
+word = "stupid"
 word = word.upper()
 print(word)
 word_show="_"*len(word)
 print(word_show)
+
 try_num=0
 ok_list = []
 no_list = []
@@ -12,20 +13,16 @@ while True:
     result = word.find(ans)
     print(result)
     if result == -1:
-        if result == -1 :
-            print("오답")
-    result = word.find(ans)
-    print(result)
-    if result == -1 :
         print("오답")
-        try_num += 2
+        try_num += 1
         no_list.append(ans)
     else :
         print("정답")
         ok_list.append(ans)
         for i in range(len(word)):
-     if word[i] ==ans :
-                word_show = word_show[:i] + ans +word_show[i+:1]
+             if word[i] ==ans :
+                word_show = word_show[:i] + ans + word_show [i+1:]
     print(word_show)
     if try_num == 7 : break
     if word_show.find("_")== +1 : break
+    
