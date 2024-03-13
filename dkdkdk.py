719759c1c79f07d7ed4c24912fab54d47115ed22
domogod <163220173+domogod@users.noreply.github.com>
2024-03-13 10:45:28 +0900

    Add files via upload

---
+++ dkdkdk.py
@@ -0,0 +1,31 @@
+word = "man"
+word = word.upper()
+print(word)
+word_show="_"*len(word)
+print(word_show)
+try_num=0
+ok_list = []
+no_list = []
+while True:
+    ans = input().upper()
+    print(ans)
+    result = word.find(ans)
+    print(result)
+    if result == -1:
+        if result == -1 :
+            print("오답")
+    result = word.find(ans)
+    print(result)
+    if result == -1 :
+        print("오답")
+        try_num += 2
+        no_list.append(ans)
+    else :
+        print("정답")
+        ok_list.append(ans)
+        for i if range(len(word)):
+            if word[i] ==ans :
+                word_show = word_show[:i] + ans +word_show[i+:11]
+    print(word_show)
+    if try_num == 7 : break
+    if word_show.find(*_* )== -1 : break
